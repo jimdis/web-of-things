3bdafd85193826992a5a209b310d378466d01629
Jim Disenstam <jim.disenstam@gmail.com>
2020-03-09 15:17:26 +0100

    Testing temperature script

--- routes/root.js
+++ routes/root.js
@@ -5,18 +5,35 @@
 'use strict'
 const express = require('express')
 const router = express.Router()
-const { spawn } = require('child_process')
-
-const script = './scripts/test.py'
+const { PythonShell } = require('python-shell')
 
 router.route('/').get(async (req, res, next) => {
   try {
-    let scriptOutput = ''
-    const python = spawn('python', [script])
-    python.stdout.on('data', data => (scriptOutput = data.toString()))
-    python.on('close', code => {
-      console.log(code)
-      res.json({ output: scriptOutput })
+    const options = {
+      scriptPath: './scripts',
+      args: [],
+    }
+    PythonShell.run('test.py', options, (err, data) => {
+      if (err) {
+        return next(err)
+      }
+      console.log(typeof data[0])
+      res.json({ data })
+    })
+  } catch (e) {
+    console.log('CAUGHT ERROR!!!')
+    next(e)
+  }
+})
+
+router.route('/:id').get(async (req, res, next) => {
+  try {
+    PythonShell.run(`./scripts/${req.params.id}.py`, null, (err, data) => {
+      if (err) {
+        return next(err)
+      }
+      console.log(typeof data[0])
+      res.json({ data })
     })
   } catch (e) {
     next(e)
--- scripts/test.py
+++ scripts/test.py
@@ -1,1 +1,7 @@
-print('Hello world')
+import sys
+
+if len(sys.argv) > 1 and sys.argv[1] == 'hello':
+    print('Hello to you too!')
+else:
+    print('Why you not say hello friend?')
+sys.stdout.flush()
